9d40545e5f0eb9fc899a375aac2794331e53af3e
Greenskulllord <gavinfaddis1@gmail.com>
2026-01-21 15:18:21 -0800

    Gavin's Code - A json file generator for the ID's of student objects

---
+++ src/Generator.java
@@ -0,0 +1,37 @@
+import com.google.gson.Gson;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+//the Generator part of the JSON creator
+//works with the Data class
+public class Generator {
+
+    //get the library going for GSON
+    Gson gson = new Gson();
+
+    Generator (String filename) {
+        //Instructions
+        System.out.println("Type enter to continue or type 'default' to get default file");
+        //run Data class to get the data needed
+        Data data = new Data();
+
+        //get the JSON writer going
+        try (BufferedWriter bWriter = new BufferedWriter(new FileWriter(filename))) {
+
+            String line = gson.toJson(data);
+            //makes it write in the data it has
+            bWriter.write(line);
+            bWriter.newLine();
+
+        } catch (IOException e) {
+            System.out.print("issue saving file");
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+}
