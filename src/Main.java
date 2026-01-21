c3c638337586cf61699991d8565bcc50cc855507
GarrisonMoore <mooreg444@gmail.com>
2026-01-21 15:48:52 -0800

    Implement CircularList class with basic operations and integrate into Main for testing.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        new Generator("data.JSON");
+    public static void main(String[] args) throws InterruptedException {
+        //new Generator("data.JSON");
 
+        //CircularList.CircularList(args);
     }
 }
