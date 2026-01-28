378e07f28af0abaf68759896ad99ed695b3c5b00
GarrisonMoore <mooreg444@gmail.com>
2026-01-28 12:42:26 -0800

    Enhance `TimeLoop` output with formatted time display in HH:mm AM/PM format; refine variable naming for clarity.

--- src/TimeLoop.java
+++ src/TimeLoop.java
@@ -6,18 +6,26 @@ public class TimeLoop {
         String RESET = "\u001B[0m";
 
         int secondsPerDay = 86400;
-        int hoursInSeconds = 3600;
-        int DAY = 1;
+        int secondsPerHour = 3600;
+        int secondsPerMinute = 60;
+
         int STEP = 0;
+        int DAY = 1;
         double hours;
+        int minutes;
+        String AMPM;
 
         while (STEP < secondsPerDay) {
             STEP++;
-            hours = (double) STEP / hoursInSeconds;
+            hours = (double) STEP / secondsPerHour;
+            minutes = (int) (hours % 1 * 60);
+
+            if (hours >= 12) AMPM = "pm"; else AMPM = "am";
 
             System.out.print("\r Day " + DAY);
-            System.out.print("  [ Time in seconds: " + GREEN + STEP +  "s"+ RESET + " ]" );
-            System.out.printf("  [ Time in hours: "+ GREEN + String.format("%.2f",hours) +  "h"+ RESET + " ]" );
+            System.out.print("  [ Time in seconds: " + GREEN + STEP + RESET + "s" + " ]" );
+            System.out.printf("  [ Time in hours: "+ GREEN + String.format("%.2f",hours) + RESET + "h" +  " ]" );
+            System.out.print("   [ Time: " + GREEN + String.format("%02d:%02d", (int)hours, minutes % 60) + RESET + AMPM + " ]");
 
             Thread.sleep(1);
             if (STEP == secondsPerDay) {
