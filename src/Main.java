e040b2686e1465cab281fde052f40894839fdb33
GarrisonMoore <mooreg444@gmail.com>
2026-02-02 13:53:56 -0800

    Enhance `Student` and `Main`: add `arrivalTime` to `Student` class, update constructors and `toString()`, and introduce sample students to `Main` for testing.

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,22 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
-        new Generator("data.JSON");
+        //new Generator("data.JSON");
 
-        //StudentList studentList = StudentList.generateStudentList();
+        StudentList studentList = new StudentList();
 
+        studentList.insert(new Student("Ben", 1, 0, 0));
+        studentList.insert(new Student("Bob", 2, 0, 0));
+        studentList.insert(new Student("Rob", 3, 0, 0));
+        studentList.insert(new Student("Tim", 4, 0, 0));
 
+        System.out.println(studentList.size());
 
-        int variable = 0;
+        System.out.println(studentList.next());
+        System.out.println(studentList.next());
+        System.out.println(studentList.next());
+        System.out.println(studentList.next());
+        System.out.println(studentList.next());
 
-        // methods to generate a slide list and student list,
-        // use the size parameter to change the size of the lists
-        SlideList slideList = SlideList.generateSlideList(20);
-        StudentList studentList = StudentList.generateStudentList( 10);
     }
 }
