dfde03e53b91bb4bef440bef202c520ba9249e2c
shawnggraham <shawnggraham@gmail.com>
2026-02-03 21:49:56 -0800

    This is just a sample class and method to illustrate how we can pull the variables sent from the addButton click event on The form and man handle them this just outputs to the console we can delete this file later

--- src/StudentSample.java
+++ src/StudentSample.java
@@ -1,10 +1,9 @@
 public class StudentSample {
 
-            /*
-            This is just a sample class and method to illustrate how we can pull the
-             variables sent from the addButton click event on the form and man handle them
-             this just outputs to the console we can delete this file later
-            */
+//            This is just a sample class and method to illustrate how we can pull the
+//             variables sent from the addButton click event on the form and man handle them
+//             this just outputs to the console we can delete this file later
+
 
 
     public static void handleStudent(int studentId, String studentName, int tripsPerWeek) {
